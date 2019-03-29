05b5f324a1fdb9828af7c34c04adaf10b963d6b8
funkeymonkey100 <n.miller@digipen.edu>
2019-03-29 15:00:44 -0700

    New changes to level 1 and pacman logic

--- Beta Engine/Game/Source/PacManLogic.cpp
+++ Beta Engine/Game/Source/PacManLogic.cpp
@@ -1,8 +1,11 @@
 #include "stdafx.h"
 #include "PacManLogic.h"
+#include "Level1.h"
 #include <Space.h>
 #include <Collider.h>
 #include <GameObject.h>
+#include <SoundManager.h>
+#include <Engine.h>
 
 
 
@@ -12,12 +15,25 @@ void PacManCollisionHandler(GameObject & object, GameObject & other)
 	{
 		object.GetComponent<PacManLogic>()->score += object.GetComponent<PacManLogic>()->pelletScore;
 		//std::cout << object.GetComponent<PacManLogic>()->score << std::endl;
+		object.GetComponent<PacManLogic>()->pelletsLeft -= 1;
+		object.GetComponent<PacManLogic>()->soundManager->PlaySound("pac-man_chomp.wav");
+		other.Destroy();
+	}
+	if (other.GetName() == "Cherry")
+	{
+		object.GetComponent<PacManLogic>()->score += 100;
+		other.Destroy();
+	}
+	if (other.GetName() == "PowerPellet")
+	{
+		object.GetComponent<PacManLogic>()->score += 50;
 		other.Destroy();
 	}
 }
 
-PacManLogic::PacManLogic() : Component("PacManLogic"), score(0), highScore(0), pelletScore(10), powerPelletScore(50)
+PacManLogic::PacManLogic() : Component("PacManLogic"), score(0), highScore(0), pelletScore(10), powerPelletScore(50), pelletsLeft(240)
 {
+	soundManager = Engine::GetInstance().GetModule<SoundManager>();
 }
 
 Component * PacManLogic::Clone() const
@@ -30,6 +46,18 @@ void PacManLogic::Initialize()
 	GetOwner()->GetComponent<Collider>()->SetCollisionHandler(PacManCollisionHandler);
 }
 
+
+
 void PacManLogic::Update(float dt)
 {
+}
+
+int PacManLogic::GetPellets()
+{
+	return pelletsLeft;
+}
+
+void PacManLogic::SetPellets(int amountOfPellets)
+{
+	pelletsLeft = amountOfPellets;
 }
