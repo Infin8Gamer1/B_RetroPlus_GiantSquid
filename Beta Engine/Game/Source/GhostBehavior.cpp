d838c5ca426a5b3b5d0dfc85ef4f99a958e19cad
Infin8gamer1 <Jacob@mjhsoftware.com>
2019-04-03 15:18:12 -0700

    added ghost behavior base class that all ghost behaviors should derive from

---
+++ Beta Engine/Game/Source/GhostBehavior.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#include "GhostBehaviorBlue.h"
+#include <ColliderTilemap.h>
+#include <GameObject.h>
+#include <Transform.h>
+#include <Space.h>
+#include <TileMapNavigation.h>
+#include <Sprite.h>
+#include <ResourceManager.h>
+
+GhostBehavior::GhostBehavior() : Component("GhostBehavior")
+{
+	// Components
+	PacManTransform = nullptr;
+	transform = nullptr;
+	navigation = nullptr;
+	sprite = nullptr;
+	colliderTilemap = nullptr;
+
+	timer = 0;
+
+	state = GhostState::Chase;
+	previousState = state;
+
+	startPos = Vector2D();
+}
+
+void GhostBehavior::Initialize()
+{
+	PacManTransform = GetOwner()->GetSpace()->GetObjectManager().GetObjectByName("PacMan")->GetComponent<Transform>();
+	colliderTilemap = GetOwner()->GetSpace()->GetObjectManager().GetObjectByName("TileMap")->GetComponent<ColliderTilemap>();
+	sprite = GetOwner()->GetComponent<Sprite>();
+	navigation = GetOwner()->GetComponent<TileMapNavigation>();
+	transform = GetOwner()->GetComponent<Transform>();
+
+	navigation->SetTarget(colliderTilemap->ConvertWorldCordsToTileMapCords(PacManTransform->GetTranslation()));
+
+	startPos = colliderTilemap->ConvertWorldCordsToTileMapCords(transform->GetTranslation());
+}
+
+void GhostBehavior::Update(float dt)
+{
+	timer += dt;
+
+	previousState = state;
+}
+
+void GhostBehavior::SetState(GhostState _state)
+{
+	state = _state;
+}
--- Beta Engine/Game/Source/GhostBehaviorBlue.cpp
+++ Beta Engine/Game/Source/GhostBehaviorBlue.cpp
@@ -5,15 +5,11 @@
 #include <Transform.h>
 #include <Space.h>
 #include <TileMapNavigation.h>
+#include <Sprite.h>
+#include <ResourceManager.h>
 
-GhostBehaviorBlue::GhostBehaviorBlue() : Component("GhostBehaviorBlue")
+GhostBehaviorBlue::GhostBehaviorBlue() : GhostBehavior()
 {
-	// Components
-	PacManTransform = nullptr;
-	navigation = nullptr;
-	colliderTilemap = nullptr;
-
-	timer = 0;
 }
 
 Component * GhostBehaviorBlue::Clone() const
@@ -21,22 +17,73 @@ Component * GhostBehaviorBlue::Clone() const
 	return new GhostBehaviorBlue(*this);
 }
 
-void GhostBehaviorBlue::Initialize()
-{
-	PacManTransform = GetOwner()->GetSpace()->GetObjectManager().GetObjectByName("PacMan")->GetComponent<Transform>();
-	colliderTilemap = GetOwner()->GetSpace()->GetObjectManager().GetObjectByName("TileMap")->GetComponent<ColliderTilemap>();
-	navigation = GetOwner()->GetComponent<TileMapNavigation>();
-
-	navigation->SetTarget(colliderTilemap->ConvertWorldCordsToTileMapCords(PacManTransform->GetTranslation()));
-}
-
 void GhostBehaviorBlue::Update(float dt)
 {
-	timer += dt;
+	//on state change
+	//IMPORTANT needs to be before previous state gets updated in the base update function
+	if (previousState != state)
+	{
+		switch (state)
+		{
+		case Dead:
+			//sprite->SetSpriteSource(ResourceManager::GetInstance().GetSpriteSource("GhostEyes", true));
+			navigation->SetTarget(colliderTilemap->ConvertWorldCordsToTileMapCords(PacManTransform->GetTranslation()));
+			break;
+		case Chase:
+			sprite->SetSpriteSource(ResourceManager::GetInstance().GetSpriteSource("GhostBlue", true));
+			break;
+		case Scatter:
+			sprite->SetSpriteSource(ResourceManager::GetInstance().GetSpriteSource("GhostBlue", true));
+			break;
+		case Frightened:
+			sprite->SetSpriteSource(ResourceManager::GetInstance().GetSpriteSource("GhostPower", true));
+			break;
+		default:
+			break;
+		}
+	}
 
+	//updates previous state and timer
+	GhostBehavior::Update(dt);
+
+	//on timer update
 	if (timer > 0.2f)
 	{
-		navigation->SetTarget(colliderTilemap->ConvertWorldCordsToTileMapCords(PacManTransform->GetTranslation()));
 		timer = 0;
+
+		switch (state)
+		{
+		case Dead:
+			if (colliderTilemap->ConvertWorldCordsToTileMapCords(transform->GetTranslation()).Distance(startPos) < 0.1)
+			{
+				state = Chase;
+			}
+			break;
+		case Chase:
+			navigation->SetTarget(colliderTilemap->ConvertWorldCordsToTileMapCords(PacManTransform->GetTranslation()));
+			break;
+		case Scatter:
+			break;
+		case Frightened:
+			break;
+		default:
+			break;
+		}
+		
+	}
+
+	//every frame
+	switch (state)
+	{
+	case Dead:
+		break;
+	case Chase:
+		break;
+	case Scatter:
+		break;
+	case Frightened:
+		break;
+	default:
+		break;
 	}
 }
